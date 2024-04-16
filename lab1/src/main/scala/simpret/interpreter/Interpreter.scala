27b5d891ab569c252685a8d818613733f6f25f7a
Anas Almawed <anas.almawed99@hotmail.com>
2024-04-16 17:49:10 +0200

    Pass all test cases

--- lab1/src/main/scala/simpret/interpreter/Interpreter.scala
+++ lab1/src/main/scala/simpret/interpreter/Interpreter.scala
@@ -22,50 +22,81 @@ object Interpreter {
 
   /* evaluation function for taking one step at a time */
   def step(tree: AST): Option[AST] = tree match{
-      /* case Variable(id) => 
-        println("here at variable id")
-        return Some(Variable(id))
-      case BoolLit(b) =>
-        println("here at bool b, " + b)
-        return Some(BoolLit(b))
-      case IntLit(i) =>
-        println("here at intlit i, " + i)
-        return Some(IntLit(i)) */
 
       case CondExp(c, e1, e2) if !isvalue(c) =>
         step(c).map(newC => CondExp(newC, e1, e2))
+
       case CondExp(BoolLit(true), e1, _) =>
-        Some(e1) // If the condition is true, evaluate to the first branch
+        Some(e1) 
+
       case CondExp(BoolLit(false), _, e2) =>
-        Some(e2) // If the condition is false, evaluate to the second branch
+        Some(e2) 
       
       case PlusExp(IntLit(n1), IntLit(n2)) => 
-        println("here at base expression for plusexp " + "n1: " + n1 + " n2: " + n2)
         Some(IntLit(n1 + n2))
+
       case PlusExp(e1, e2) if !isvalue(e1) => 
-        println("here at e1 is not evaluable at plusexp, e1: " + e1)
         step(e1).map(newE1 => PlusExp(newE1, e2))
+
       case PlusExp(e1, e2) if !isvalue(e2) => 
-        println("here at e2 is not evaluable at plusexp, e2: " + e2)
         step(e2).map(newE2 => PlusExp(e1, newE2)) 
 
       case IsZeroExp(e) if !isvalue(e) =>
-        println("here at e is not evaluable at iszeroexp, e: " + e)
         step(e).map(newE => IsZeroExp(newE))
+
       case IsZeroExp(IntLit(i)) => 
-        Some(BoolLit(i == 0)) 
+        Some(BoolLit(i == 0))
 
-      case LamExp(id, e) if !isvalue(e) =>
-        println("here at e is not evaluable at lamexp, e: " + e + " id: " + id)
-        Some(e)
+      case AppExp(e1, e2) if !isvalue(e1)  =>
+        step(e1).map(newE1 => AppExp(newE1, e2))
 
-      case _ => None // No further steps can be taken
+      case AppExp(e1, e2) if !isvalue(e2)  =>
+        step(e2).map(newE2 => AppExp(e1, newE2))
+
+      case AppExp(LamExp(id, body), v2) if isvalue(v2) =>
+        Some(substitute(body, id, v2))
+
+      case _ => None 
   }
 
   /* evaluation function to iterate the steps of evaluation */
   def eval(x: AST): AST = step(x) match {
     case None => x
     case Some(x1) => eval(x1)
+  }
+
+  def substitute(expr: AST, id: String, value: AST): AST = expr match {
+    case Variable(varId) if varId == id =>
+      value  
+
+    case Variable(_) =>
+      expr  
+
+    case BoolLit(_) | IntLit(_) =>
+      expr 
+
+    case CondExp(cond, e1, e2) =>
+      CondExp(substitute(cond, id, value), substitute(e1, id, value), substitute(e2, id, value))
+
+    case IsZeroExp(e) =>
+      IsZeroExp(substitute(e, id, value))
+
+    case PlusExp(e1, e2) =>
+      PlusExp(substitute(e1, id, value), substitute(e2, id, value))
+
+
+    case LamExp(varId, body) =>
+      if (freeVars(value).contains(varId)) {
+        // Variable `varId` is in the set of free variables of `value`, need renaming
+        val newId = newVarId(varId, freeVars(body) ++ freeVars(value))
+        val renamedBody = substitute(body, varId, Variable(newId))
+        LamExp(newId, substitute(renamedBody, id, value))
+      } else {
+        LamExp(varId, substitute(body, id, value))
+      }
+
+    case AppExp(e1, e2) =>
+      AppExp(substitute(e1, id, value), substitute(e2, id, value))
   }
 
   /* function to apply the interpreter */
@@ -77,5 +108,31 @@ object Interpreter {
         val msg2 = msg + " -> \r\n" + ASTPrinter.convToStr(xe)
         Left(EvaluationError(Location.fromPos(xe.pos), msg2))
     }
+  }
+
+  def freeVars(expr: AST): Set[String] = expr match {
+    case Variable(id) => 
+      Set(id)
+    case BoolLit(_) | IntLit(_) => 
+      Set.empty
+    case CondExp(cond, e1, e2) => 
+      freeVars(cond) ++ freeVars(e1) ++ freeVars(e2)
+    case IsZeroExp(e) => 
+      freeVars(e)
+    case PlusExp(e1, e2) => 
+      freeVars(e1) ++ freeVars(e2)
+    case LamExp(id, body) => 
+      freeVars(body) - id
+    case AppExp(e1, e2) => 
+      freeVars(e1) ++ freeVars(e2)
+  }
+
+  def newVarId(oldId: String, value: Set[String]): String = {
+    var suffix = "_"
+    var newId = oldId + suffix
+    if (value.contains(newId)) {
+      newId = newId + suffix
+    }
+    newId
   }
 }
